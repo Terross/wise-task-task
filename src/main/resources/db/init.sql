44ea34eb05fb6ec5e0be93a9b09483b02a59bbf2
Terross <dima38091@gmail.com>
2023-08-21 23:49:34 +0300

    Move migrations

--- src/main/resources/db/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER wise_task_task WITH PASSWORD 'wise_task_task';
-CREATE DATABASE wise_task_task WITH OWNER = wise_task_task;
-CREATE SCHEMA wise_task_task;
-
--- docker run --name profile -e POSTGRES_PASSWORD=wise_task_task -e POSTGRES_DB=wise_task_task -e POSTGRES_USER=wise_task_task -p 5434:5432 -d postgres
